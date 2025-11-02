da5c1a7e6a6d1fbdee897099a07686233748575a
Matthew <thehelleye@gmail.com>
2025-11-02 16:42:14 +0100

    Add editorconfig and format code

--- Program.cs
+++ Program.cs
@@ -7,16 +7,14 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-  app.MapOpenApi();
+if (app.Environment.IsDevelopment()) {
+	app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
 
-app.MapGet("/docs", () =>
-{
-  return Results.File(File.OpenRead("./Documentation/stoplightio.html"), "text/html");
+app.MapGet("/docs", () => {
+	return Results.File(File.OpenRead("./Documentation/stoplightio.html"), "text/html");
 })
 .WithName("Documentation")
 .WithDescription("Show API documentation using stoplightio");
