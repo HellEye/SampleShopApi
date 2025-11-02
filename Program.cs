015f4749332e517f11bef0a6882c9fe718ee26c0
Matthew <thehelleye@gmail.com>
2025-11-02 16:38:33 +0100

    Remove demo code

--- Program.cs
+++ Program.cs
@@ -14,10 +14,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 app.MapGet("/docs", () =>
 {
   return Results.File(File.OpenRead("./Documentation/stoplightio.html"), "text/html");
@@ -25,23 +21,5 @@ app.MapGet("/docs", () =>
 .WithName("Documentation")
 .WithDescription("Show API documentation using stoplightio");
 
-app.MapGet("/weatherforecast", () =>
-{
-  var forecast = Enumerable.Range(1, 5).Select(index =>
-      new WeatherForecast
-      (
-          DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-          Random.Shared.Next(-20, 55),
-          summaries[Random.Shared.Next(summaries.Length)]
-      ))
-      .ToArray();
-  return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-  public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
