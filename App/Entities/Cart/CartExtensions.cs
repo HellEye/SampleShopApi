0b21304140386b1e9fdffbed6b98779e14f23ab3
Matthew <thehelleye@gmail.com>
2026-01-14 16:38:27 +0100

    Add secure to cookie

--- App/Entities/Cart/CartExtensions.cs
+++ App/Entities/Cart/CartExtensions.cs
@@ -5,7 +5,7 @@ public static class CartExtensions {
 		response.Cookies.Append("cartId", cartId.ToString(), new CookieOptions {
 			SameSite = SameSiteMode.None,
 			Expires = DateTimeOffset.UtcNow.AddDays(7),
-			Secure = false,
+			Secure = true,
 		});
 		response.Headers.Append("access-control-expose-headers", "Set-Cookie");
 	}
